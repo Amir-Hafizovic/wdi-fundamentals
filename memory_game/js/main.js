9c0bfe2088679fdabb3d276c3c21e7bbfbd21bf2
Amir Hafizovic <amirhafizovic@Ayshas-MBP.lan>
2017-12-02 12:59:59 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,11 +3,22 @@ var cardsInPlay = [];
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+}
 
 
 
 
 
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
 //console.log("User flipped " + cardThree);
