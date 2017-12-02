c995e648a74005129c6362a14c747eb8b6b265f6
Amir Hafizovic <amirhafizovic@Ayshas-MBP.lan>
2017-12-02 12:34:53 +1100

    added new files to repo

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+
+
+
+
+
+
+console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
