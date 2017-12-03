2f8df71da6183f972fa59a33a3c3c4a6522cf463
Amir Hafizovic <amirhafizovic@Ayshas-MBP.lan>
2017-12-03 13:58:02 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,31 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again.");
+  console.log("You found a match!");
+  } else {
+  console.log("Sorry, try again.");
   }
 }
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId])
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+flipCard(0);
+flipCard(2);
+/*
 
 
 
-
-
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
-//console.log("User flipped " + cardThree);
+
+*/
