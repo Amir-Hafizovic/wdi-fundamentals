93b9ff12e03efbfc21a8937b5183485f662c5727
Amir Hafizovic <hafizovic.amir@gmail.com>
2017-12-03 20:59:44 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,36 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png",
+  },
+  {
+  rank: "queen",
+  suit:"diamonds",
+  cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png",
+  },
+  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png",
+  }
+];
 var cardsInPlay = [];
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
   } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
   }
 }
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId])
+  console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit + " @ " + cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
@@ -17,7 +38,7 @@ function flipCard(cardId) {
 flipCard(0);
 flipCard(2);
 /*
-
+cards[cardId].rank
 
 
 var cardOne = cards[0];
